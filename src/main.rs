d3dcbd66cd5c80cb291983ab7715dee441f27661
Daniel R <nzdanron@gmail.com>
2023-02-21 14:21:52 +1300

    Added CLI Options
    Using the command line the user can now change the time delay between
    char iterations and what phrase to attempt to print

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+use clap::Parser;
 use std::{thread, time};
 
+/// That fancy instagram animation for hello world in rust
+#[derive(Parser)]
+struct Args {
+    // The phrase to print
+    phrase: Option<String>,
+    /// The milliseconds taken inbetween char iterations
+    sleep_time: Option<u64>,
+}
+
 fn main() {
-    let wanted_phrase = "Hello, world!".to_string();
+    let args = Args::parse();
+
+    let wanted_phrase = args.phrase.unwrap_or("Hello, world!".to_string());
+
+    let time_to_sleep = time::Duration::from_millis(args.sleep_time.unwrap_or(2));
 
     let mut phrase = String::new();
-
-    let time_to_sleep = time::Duration::from_millis(2);
 
     for wanted_char in wanted_phrase.chars() {
         let mut current_letter_as_num: u8 = ' ' as u8;
